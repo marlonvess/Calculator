00960d6ffa592b7cefb049be093547efd3b6caac
marlonvess <marlonvess@gmail.com>
2024-04-14 18:33:53 +0100

    additional comments

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const display = document.getElementById("calcDisplay");
-
+ //get numbers from "onclick" on index page and store it in key parameter.
 function showResults(key) {
     if (key === "=") {
-        let expression = display.value;
-        let result = evaluateExpression(expression);
-        display.value = result;
+        let expression = display.value; 
+        let result = evaluateExpression(expression); 
+        display.value = result; //In case = key be used, it will sum all values inserted with this function
     } else {
-        display.value += key;
+        display.value += key; //IF key is different from = it will keep add numbers in display
     }
 }
 
